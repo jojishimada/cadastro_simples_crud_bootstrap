43205d36b36a3ba255e8b8b103ea6b55dd62cdda
Joji Shimada <jojishimada@gmail.com>
2023-10-15 15:17:25 -0300

    v3 final

--- salvar-usuario.php
+++ salvar-usuario.php
@@ -35,7 +35,16 @@
             }
             break;
         case 'excluir':
-            // code...
+            $sql = "DELETE FROM usuarios WHERE id=".$_REQUEST["id"];
+            $res =$conn->query($sql);
+
+            if($res==true){
+                print "<script>alert('Excluido com sucesso');</script>";
+                print "<script>location.href='?page=listar';</script>";           
+            }else{
+                print "<script>alert('Não foi possível Excluir');</script>";
+                print "<script>location.href='?page=listar';</script>";   
+            }
             break;
 
     }
